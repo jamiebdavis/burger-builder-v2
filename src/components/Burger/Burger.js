1d427247650303d4b016fe2e82ee8dc5873b0c9a
jamiebdavis <jamied1985@gmail.com>
2019-02-18 18:10:06 +0000

    calculated ingredients dynamically - lesson 155

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,25 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please Start Adding Ingredients</p>;
+  }
+  console.log(transformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
