fcae08f1df8c0ee6abee59014713c30a1b41080f
jamiebdavis <jamied1985@gmail.com>
2019-02-23 16:35:13 +0000

    added sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import Aux from "../../hoc/AuxWrap";
 import classes from "./Layout.module.css";
 import BackDrop from "../UI/Backdrop/Backdrop";
 import Toolbar from "../navigation/Toolbar/Toolbar";
+import SideDraw from "../navigation/sidedraw/SideDraw";
 
 const Layout = props => {
   return (
     <Aux>
       <Toolbar />
+      <SideDraw />
       <div>
         <div>toolbar, sidedraw, backdrop</div>
         <main className={classes.Content}>{props.children}</main>
--- src/components/navigation/Toolbar/Toolbar.js
+++ src/components/navigation/Toolbar/Toolbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
 
-    <Logo />
-
-    <nav>...</nav>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
