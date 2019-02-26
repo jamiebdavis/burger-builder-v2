0f1f84e64b4495a33195fe04adf1a301547ce444
jamiebdavis <jamied1985@gmail.com>
2019-02-26 13:07:43 +0000

    added routing to web app - react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import CheckOut from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          {" "}
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={CheckOut} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
